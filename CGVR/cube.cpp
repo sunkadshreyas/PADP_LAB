1e87264c90cdef588c8f65f58af1ef1ea0dcd015
Shreyas S <sunkad.shreyas@gmail.com>
2021-12-14 17:50:29 +0530

    updated cube cpp

--- CGVR/cube.cpp
+++ CGVR/cube.cpp
@@ -1,60 +1,67 @@
-#include <stdlib.h>
-#include <GL/glut.h>
-#include<gl\GL.h>
-#include<gl\GLU.h>
-#include <time.h>
-
-GLfloat vertices[] = { -1.0,-1.0,-1.0,1.0,-1.0,-1.0,
-1.0,1.0,-1.0, -1.0,1.0,-1.0, -1.0,-1.0,1.0,
-1.0,-1.0,1.0, 1.0,1.0,1.0, -1.0,1.0,1.0 };
-
-GLfloat normals[] = { -1.0,-1.0,-1.0,1.0,-1.0,-1.0,
-1.0,1.0,-1.0, -1.0,1.0,-1.0, -1.0,-1.0,1.0,
-1.0,-1.0,1.0, 1.0,1.0,1.0, -1.0,1.0,1.0 };
-
-GLfloat colors[] = { 0.0,0.0,0.0,1.0,0.0,0.0,
-1.0,1.0,0.0, 0.0,1.0,0.0, 0.0,0.0,1.0,
-1.0,0.0,1.0, 1.0,1.0,1.0, 0.0,1.0,1.0 };
-
-GLubyte cubeIndices[] = { 0,3,2,1,2,3,7,6,0,4,7,3,1,2,6,5,4,5,6,7,0,1,5,4 };
-
-static GLfloat theta[] = { 0.0,0.0,0.0 };
-static GLfloat beta[] = { 0.0,0.0,0.0 };
-static GLint axis = 2;
-
-void delay(float secs)
+#include<stdio.h>
+#include<gl/glut.h>
+GLfloat vertices[][3] =
 {
-	float end = clock() / CLOCKS_PER_SEC + secs;
-	while ((clock() / CLOCKS_PER_SEC) < end);
+{-1.0,-1.0,-1.0},{1.0,-1.0,-1.0},
+{1.0,1.0,-1.0},{-1.0,1.0,-1.0},
+{-1.0,-1.0,1.0},{1.0,-1.0,1.0},
+{1.0,1.0,1.0},{-1.0,1.0,1.0}
+};
+GLfloat colors[][3] =
+{
+{0.0,0.0,0.0},{0,0.0,0.0},
+{1.0,1.0,0.0},{0.0,1.0,1.0},
+{0.0,0.0,1.0},{1.0,0.0,1.0},
+{1.0,1.0,1.0},{0.0,1.0,1.0}
+};
+
+
+void polygon(int a, int b, int c, int d)
+{
+	glBegin(GL_POLYGON);
+	glColor3fv(colors[a]);
+	glVertex3fv(vertices[a]);
+	glColor3fv(colors[b]);
+	glVertex3fv(vertices[b]);
+	glColor3fv(colors[c]);
+	glVertex3fv(vertices[c]);
+	glColor3fv(colors[d]);
+	glVertex3fv(vertices[d]);
+
+	glEnd();
 }
 
-void displaySingle(void)
-{
-	/* display callback, clear frame buffer and z buffer,
-	   rotate cube and draw, swap buffers */
 
+void colorcube(void)
+{
+	polygon(0, 3, 2, 1);
+	polygon(2, 3, 7, 6);
+	polygon(0, 4, 7, 3);
+	polygon(1, 2, 6, 5);
+	polygon(4, 5, 6, 7);
+	polygon(0, 1, 5, 4);
+	//similarly for remaining 5 faces
+}
+static GLfloat theta[] = { 0.0,0.0,0.0 };//angle of rotation along each axis
+static GLint axis = 2;//current axis
+
+
+void display(void)
+{
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glLoadIdentity();
-	glRotatef(theta[0], 1.0, 0.0, 0.0);
+	glRotatef(theta[0], 1.0, 0.0, 0.0);//for x axis
 	glRotatef(theta[1], 0.0, 1.0, 0.0);
 	glRotatef(theta[2], 0.0, 0.0, 1.0);
-
-	glDrawElements(GL_QUADS, 24, GL_UNSIGNED_BYTE, cubeIndices);
-
-	glBegin(GL_LINES);
-	glVertex3f(0.0, 0.0, 0.0);
-	glVertex3f(1.0, 1.0, 1.0);
-	glEnd();
-
-	glFlush();
+	// similarly for y and z axis
+	colorcube();
+	glutSwapBuffers();
 }
+
 
 void spinCube()
 {
-	/* Idle callback, spin cube 2 degrees about selected axis */
-	//sleep(50);
-	delay(0.01);
-	theta[axis] += 2.0;
+	theta[axis] += 0.025;
 	if (theta[axis] > 360.0) theta[axis] -= 360.0;
 	glutPostRedisplay();
 }
@@ -62,23 +69,22 @@ void spinCube()
 
 void mouse(int btn, int state, int x, int y)
 {
-	/* mouse callback, selects an axis about which to rotate */
-	if (btn == GLUT_LEFT_BUTTON && state == GLUT_DOWN) axis = 0;
-	if (btn == GLUT_MIDDLE_BUTTON && state == GLUT_DOWN) axis = 1;
-	if (btn == GLUT_RIGHT_BUTTON && state == GLUT_DOWN) axis = 2;
+	if (btn == GLUT_LEFT_BUTTON && state == GLUT_DOWN)axis = 0;//x axis rotation
+	if (btn == GLUT_MIDDLE_BUTTON && state == GLUT_DOWN)axis = 1;//y axis rotn
+	if (btn == GLUT_RIGHT_BUTTON && state == GLUT_DOWN)axis = 2;
+	//similarly for y and z axis
 }
 
-void myReshape(int w, int h)
+
+void myReshape(int w, int h)// common reshape func for all prgrms
 {
 	glViewport(0, 0, w, h);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	if (w <= h)
-		glOrtho(-2.0, 2.0, -2.0 * (GLfloat)h / (GLfloat)w,
-			2.0 * (GLfloat)h / (GLfloat)w, -10.0, 10.0);
+		glOrtho(-2.0, 2.0, -2.0 * (GLfloat)h / (GLfloat)w, 2.0 * (GLfloat)h / (GLfloat)w, -10.0, 10.0);
 	else
-		glOrtho(-2.0 * (GLfloat)w / (GLfloat)h,
-			2.0 * (GLfloat)w / (GLfloat)h, -2.0, 2.0, -10.0, 10.0);
+		glOrtho(-2.0 * (GLfloat)w / (GLfloat)h, 2.0 * (GLfloat)w / (GLfloat)h, -2.0, 2.0, -10.0, 10.0);
 	glMatrixMode(GL_MODELVIEW);
 }
 
@@ -86,22 +92,14 @@ void myReshape(int w, int h)
 int main(int argc, char** argv)
 {
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowPosition(100, 100);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 	glutInitWindowSize(500, 500);
-	glutCreateWindow("colorcube");
+	glutCreateWindow("color cube viewer");
 	glutReshapeFunc(myReshape);
-	glutDisplayFunc(displaySingle);
-	glutIdleFunc(spinCube);
+	glutDisplayFunc(display);
 	glutMouseFunc(mouse);
-	glEnable(GL_DEPTH_TEST); /* Enable hidden--surface--removal */
-	glEnableClientState(GL_COLOR_ARRAY);
-	glEnableClientState(GL_NORMAL_ARRAY);
-	glEnableClientState(GL_VERTEX_ARRAY);
-	glVertexPointer(3, GL_FLOAT, 0, vertices);
-	glColorPointer(3, GL_FLOAT, 0, colors);
-	glNormalPointer(GL_FLOAT, 0, normals);
-	glColor3f(1.0, 1.0, 1.0);
+	glutIdleFunc(spinCube);
+	glEnable(GL_DEPTH_TEST);
 	glutMainLoop();
-
+	return 0;
 }
